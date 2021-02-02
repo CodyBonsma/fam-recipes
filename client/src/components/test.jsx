a646e0ec2a89d96298fa76e5fb328e9eaf7b6b80
Cody Bonsma <codybonsma@gmail.com>
2021-02-01 20:30:06 -0500

    setup complete

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const test = () => {
-    return (
-        <div>
-            <h2>This will hold come recipes!</h2>
-        </div>
-    );
+  const meals = [
+    {
+      apps: "Chips n queso",
+      entree: ["Cuban Especial", "Burrito", "Tacos", "quesadilla"],
+      side: "plantains",
+      desert: "ice-cream",
+    },
+  ];
+  return (
+    <div>
+      <h2>This will hold come recipes!</h2>
+
+      {meals.map((food) => {
+        return (
+          <ul>
+            <li>{food.entree}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
 };
 
 export default test;
