8fe71d63c800eb375358d0100cc95994338979b9
Cody Bonsma <codybonsma@gmail.com>
2021-02-01 19:49:52 -0500

    setup server file and connecting backend

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-
+// set server PORT
 const PORT = process.env.PORT || 3001;
+
+app.get("/api/config", (req, res) => {
+  res.json({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
