049eefd3e92e58213852ae0054d0be1cecb62887
Cody Bonsma <codybonsma@gmail.com>
2021-02-04 19:47:08 -0500

    linking modal to portal

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal/Modal";
 import "./Home.css";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-12 home-header">
           <h2>This will be the homepage</h2>
-          <button>Add Recipe</button>
+          <Modal open={isOpen}>
+            <h3>this will hold a form</h3>
+            <button onClick={() => setIsOpen(false)}>close</button>
+          </Modal>
+          <button onClick={() => setIsOpen(true)}>Add Recipe</button>
         </div>
       </div>
     </div>
---
+++ client/src/components/Modal/Modal.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Modal = ({ children, open }) => {
+  if (!open) return null;
+  return <div>{children}</div>;
+};
+
+export default Modal;
