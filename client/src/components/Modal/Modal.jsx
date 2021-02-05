73cf13759a7045355245a202c45003eef06a9a5d
Cody Bonsma <codybonsma@gmail.com>
2021-02-04 20:05:06 -0500

    modal styling

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import "./Modal.css";
+import ReactDom from "react-dom";
 
 const Modal = ({ children, open }) => {
   if (!open) return null;
-  return <div>{children}</div>;
+  return ReactDom.createPortal(
+    <>
+      <div className="overlay-dis" />
+      <div className="modal-dis">{children}</div>
+    </>,
+    document.getElementById("portal")
+  );
 };
 
 export default Modal;
