8fd3c1dc7d28be4af10b9b73615db4e38425ff46
Cody Bonsma <codybonsma@gmail.com>
2021-02-04 20:28:50 -0500

    form

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,7 +10,39 @@ const Home = () => {
         <div className="col-sm-12 home-header">
           <h2>This will be the homepage</h2>
           <Modal open={isOpen}>
-            <h3>this will hold a form</h3>
+            <div class="mb-3">
+              <label for="formGroupExampleInput" className="form-label">
+                Recipe Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput"
+                placeholder="What's cooking"
+              />
+            </div>
+            <div className="mb-3">
+              <label for="formGroupExampleInput2" className="form-label">
+                Ingredients (comma separated)
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="formGroupExampleInput2"
+                placeholder="garlic, parsley..."
+              />
+            </div>
+            <label for="inputPassword5" className="form-label">
+              Description
+            </label>
+            <input
+              type="description"
+              id="recipe-description"
+              className="form-control"
+            />
+            <div id="recipe-helpBlock" className="form-text">
+              Describe the process to get this delicious dish ready and served
+            </div>
             <button onClick={() => setIsOpen(false)}>close</button>
           </Modal>
           <button onClick={() => setIsOpen(true)}>Add Recipe</button>
