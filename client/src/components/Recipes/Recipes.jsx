33eca0d2dfbdbfcb81229dfe8f44c3fee10f6c2c
Cody Bonsma <codybonsma@gmail.com>
2021-02-10 15:49:54 -0500

    adding delete funcitonality for found recipes

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -2,25 +2,54 @@ import React, { useEffect, useState } from "react";
 import Data from "../../utils/connect";
 
 const Recipes = () => {
-  const [savedRecipes, setSavedRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState();
 
   // grab saved recipes when page loads
   useEffect(() => {
     Data.savedRecipes({})
       .then((foundRecipes) => {
-        console.log("WHAT I FOUND", foundRecipes);
-        setSavedRecipes(foundRecipes);
+        console.log("WHAT I FOUND", foundRecipes.data);
+        if(foundRecipes){
+            setSavedRecipes(foundRecipes.data);
+
+        } else {
+            setSavedRecipes('')
+        }
       })
       .catch((err) => {
         if (err) throw err;
       });
   }, []);
 
-  
   return (
-    <div>
-      <h2>This will hold the saved recipes</h2>
-    </div>
+    <>
+      <div>
+        <h2>This will hold the saved recipes</h2>
+        <ul>
+          {savedRecipes ? (
+            savedRecipes.map((recipe) => {
+              return (
+                <div>
+                  <li>
+                    <b>{recipe.name}</b>
+                  </li>
+                  <label>Ingredients: </label>
+                  <li>{recipe.ingredients}</li>
+                  <p>{recipe.description}</p>
+                  <br />
+                  <button>delete</button>
+                  <hr />
+                </div>
+              );
+            })
+          ) : (
+            <div>
+              <p>No saved recipes to show</p>
+            </div>
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
--- client/src/utils/connect.js
+++ client/src/utils/connect.js
@@ -2,12 +2,10 @@ import axios from "axios";
 
 export default {
   saveRecipe: (recipe) => {
-    console.log("SENDING recipe: ", recipe);
     return axios.post("/api/recipe", recipe);
   },
 
   savedRecipes: (savedRecipe) => {
-    console.log("Requesting recipes: ", savedRecipe);
     return axios.get("/api/recipe", savedRecipe);
   },
 };
