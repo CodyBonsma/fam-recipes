a91cc598b04603786e68349a0dcfefbdcb948f9a
Cody Bonsma <codybonsma@gmail.com>
2021-03-01 20:35:11 -0500

    mapping over saved data from API

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Search.css";
 
 const CORS = "https://serene-badlands-79714.herokuapp.com/";
 const SEARCHURL = "https://api.edamam.com/search?q=";
+const APP_ID = "cb5d3f03";
+const APP_KEY = "f3fc16a2bf8ec34b785bef544190e060";
 
 const Search = () => {
+  const [list, setList] = useState('');
+
   const getRecipes = () => {
     return axios
       .get(
         CORS +
           SEARCHURL +
           "chicken" +
-          "&app_id=cb5d3f03" +
-          "&app_key=f3fc16a2bf8ec34b785bef544190e060"
+          "&app_id=" +
+          APP_ID +
+          "&app_key=" +
+          APP_KEY +
+          "&from=0&to=5"
       )
       .then((res) => {
-        console.log(res);
+        console.log(res.data.hits);
+        setList(res.data.hits);
+        console.log(list)
       })
       .catch((err) => {
         if (err) throw err;
@@ -27,7 +36,38 @@ const Search = () => {
     <div className="edamam-section">
       <h3>This will hole the edamam API info</h3>
       <button onClick={() => getRecipes()}>click to get recipes</button>
-      <input></input>
+      {list ? (
+        list.map((item) => {
+          return (
+            <div className="card mb-3" style="max-width: 540px;">
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src="..." alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.recipe.ingredients.label}</h5>
+                    <p className="card-text">
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This content is a
+                      little bit longer.
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">
+                        Last updated 3 mins ago
+                      </small>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <h3>Nothing to show</h3>
+        </div>
+      )}
     </div>
   );
 };
