070256a44eaf3e56c9cc890a8ba124e4f8153151
Cody Bonsma <codybonsma@gmail.com>
2021-03-14 16:15:37 -0400

    creating expanded recipe box

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import SavedRecipes from "./components/Recipes/Recipes";
+import Expanded from "./components/Expanded/Expanded"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route exact path={["/savedrecipes"]}>
             <SavedRecipes />
+          </Route>
+          <Route>
+            <Expanded exact path={["/expandedrecipe"]} />
           </Route>
         </Switch>
       </div>
---
+++ client/src/components/Expanded/Expanded.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Expanded.css"
+
+const Expanded = () => {
+    return (
+        <div className="container-main">
+            <h3>this will hold the clicked recipe information</h3>
+            <p>HELLOOOOOO</p>
+        </div>
+    );
+};
+
+export default Expanded;
